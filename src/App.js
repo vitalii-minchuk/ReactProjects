41e988b8b9fb04ccf0348e6a27a036d65f38bd45
vitalii-minchuk <mcmin4doutsch@gmail.com>
2022-04-06 00:48:16 +0300

    add some functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
-  
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFileteredTodos] = useState([]);
+
+  useEffect(() => {
+    filterHandler()
+  }, [todos, status]);
+
+  const filterHandler = () => {
+    
+    switch (status) {
+      case "completed":
+        setFileteredTodos(todos.filter(todo => todo.completed === true));
+        break;
+
+      case "uncompleted":
+        setFileteredTodos(todos.filter(todo => todo.completed === false));
+        break;
+    
+      default:
+        setFileteredTodos(todos)
+        break;
+    }
+  }
+
   return (
     <div className="App">
       <header>
         <h1>My Todo List</h1>
       </header>
-      <Form />
-      <TodoList />
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+        setStatus={setStatus}
+      />
+      <TodoList
+        filteredTodos={filteredTodos}
+        setTodos={setTodos}
+        todos={todos}
+      />
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,22 @@
 import React from "react";
+import Todo from "./Todo";
 
 
-const TodoList = () => {
+const TodoList = ({todos, setTodos, filteredTodos}) => {
+
   return (
     <div className="todo-container">
-      <ul className="todo-list"></ul>
+      <ul className="todo-list">
+        {filteredTodos.map((todo) => (
+          <Todo
+            setTodos={setTodos}
+            todos={todos}
+            todo={todo}
+            key={todo.id}
+            text={todo.text}
+          />
+        ))}
+      </ul>
     </div>
   );
 };
