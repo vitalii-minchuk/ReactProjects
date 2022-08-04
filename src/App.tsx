3757889373de2b2478f0e01edc458f4153f6985c
vitalii-minchuk <mcmin4doutsch@gmail.com>
2022-08-04 22:04:26 +0300

    basic structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Flex } from "@chakra-ui/react";
+import { FC } from "react";
+
+
+const App: FC = () => {
+  return (
+    <>
+    <Flex>hello</Flex>
+    </>
+  );
+}
+
+export default App;
